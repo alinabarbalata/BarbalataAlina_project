f7182c5b1b469b3c66a24f1b6b77fd566740e7c2
alinabarbalata <barbalataalina21@stud.ase.ro>
2022-12-02 02:33:50 +0200

    1st set of tests

--- BarbalataAlina_project/Main.cpp
+++ BarbalataAlina_project/Main.cpp
@@ -5,4 +5,25 @@ using namespace std;
 
 int main() {
 	Event location1;
+	string zones[] = { "A","B","C" };
+	location1.setZonesAndNoZones(zones, 3);
+	int* vector = nullptr;
+	cout << endl << "The number of rows is: " << location1.getNoRowsPerZone() << endl;
+	for (int i = 0; i < location1.getNoZones(); i++) {
+		cout << endl << Event::lengthString(location1.getZones()[i]);
+	}
+	cout << endl;
+	for (int i = 0; i < location1.getNoZones(); i++) {
+		cout << location1.getZones()[i] << " ";
+	}
+	cout << endl;
+	zones[2] = "VIP";
+	location1.setZonesAndNoZones(zones, 3);
+	for (int i = 0; i < location1.getNoZones(); i++) {
+		cout << location1.getZones()[i] << " ";
+	}
+	for (int i = 0; i < location1.getNoZones(); i++) {
+		cout << endl << Event::lengthString(location1.getZones()[i]);
+	}
+
 }
