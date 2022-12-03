06b59d2f1acaf8f28b5f4b6f2f925f91c8620229
alinabarbalata <barbalataalina21@stud.ase.ro>
2022-12-03 17:19:56 +0200

    Class3: setters and getters

--- BarbalataAlina_project/PersonInfo.h
+++ BarbalataAlina_project/PersonInfo.h
@@ -6,37 +6,39 @@ using namespace std;
 class PersonInfo {
 private:
 	int idPerson = 0;
-	string name="-";
-	char* dateBirth = nullptr;
-	string phoneNo;
+	string name = "-";
+	int dayBirth = 0;
+	int monthBirth = 0;
+	int yearBirth = 0;
 
 	const static int NO_DIGITS = 10;
 	const static int NO_CHARACTERS_DATE = 8;
 public:
-	PersonInfo(const string name, char* dateBirth, string phoneNo) {
-		int ok = 1;
+	PersonInfo(const string name) {
 		this->name = name;
-		if (strlen(dateBirth) == PersonInfo::NO_CHARACTERS_DATE) {
-			if (dateBirth[2] = '.' && dateBirth[2] == dateBirth[5])
-			{
-				if (isDigit(dateBirth[0]) && isDigit(dateBirth[1]) && isDigit(dateBirth[3])
-					&& isDigit(dateBirth[4]) && isDigit(dateBirth[6]) && isDigit(dateBirth[7])
-					&& isDigit(dateBirth[8]))
-					this->dateBirth = dateBirth;
-			}
-		}
-		else
-			throw "Invalid date of birth";
-		if (Event::lengthString(phoneNo) == PersonInfo::NO_DIGITS)
-					this->phoneNo = phoneNo;
-		else
-					throw "Invalid phone number";
-				cout << endl << "Person " << this->name << " was created";
+		cout << endl << "Person " << this->name << " was created";
 	}
 
-	static bool isDigit(char character) {
-		if (character >= 48 && character <= 57)
-			return true;
-		return false;
+
+	string getName() {
+		return this->name;
+	}
+
+	int getDayBirth() {
+		return this->dayBirth;
+	}
+
+	int getMonthBirth() {
+		return this->monthBirth;
+	}
+
+	int getYearBirth() {
+		return this->yearBirth;
+	}
+
+	void setDateBirth(int day, int month, int year) {
+		this->dayBirth = day;
+		this->monthBirth = month;
+		this->yearBirth = year;
 	}
 };
